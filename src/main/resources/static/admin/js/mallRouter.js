59474a2184838f734a282997572012929ffe6e96
xiejiangchu <xiejiangchu@163.com>
2017-03-07 22:13:29 +0800

    修改

--- src/main/resources/static/admin/js/mallRouter.js
+++ src/main/resources/static/admin/js/mallRouter.js
@@ -5,57 +5,57 @@ mall.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     };
     $stateProvider.state('dashBoard', {
         url: '/dashBoard',
-        templateUrl: '../html/dashBoard.html',
+        templateUrl: '/html/dashBoard.html',
         controller: 'dashBoardController'
     }).state('item', {
         abstract: true,
         url: '/item',
-        templateUrl: '../html/item.html',
+        templateUrl: '/html/item.html',
         controller: 'itemController'
     }).state('item.list', {
         url: '/itemList/:page',
-        templateUrl: '../html/itemList.html',
+        templateUrl: '/html/itemList.html',
         controller: 'itemListController'
     }).state('item.add', {
         url: '/itemAdd',
-        templateUrl: '../html/itemAdd.html',
+        templateUrl: '/html/itemAdd.html',
         controller: 'itemAddController'
     }).state('item.addBatch', {
         url: '/itemAddBatch',
-        templateUrl: '../html/itemAddBatch.html',
+        templateUrl: '/html/itemAddBatch.html',
         controller: 'itemAddBatchController'
     }).state('image', {
         url: '/image',
-        templateUrl: '../html/image.html',
+        templateUrl: '/html/image.html',
         controller: 'imageController'
     }).state('itemDetail', {
         url: '/itemDetail/:id',
-        templateUrl: '../html/itemDetail.html',
+        templateUrl: '/html/itemDetail.html',
         controller: 'itemDetailController'
     }).state('category', {
         abstract: true,
         url: '/category',
-        templateUrl: '../html/category.html',
+        templateUrl: '/html/category.html',
         controller: 'categoryController'
     }).state('category.list', {
         url: '/categoryList/:page',
-        templateUrl: '../html/categoryList.html',
+        templateUrl: '/html/categoryList.html',
         controller: 'categoryListController'
     }).state('category.add', {
         url: '/categoryAdd',
-        templateUrl: '../html/categoryAdd.html',
+        templateUrl: '/html/categoryAdd.html',
         controller: 'categoryAddController'
     }).state('categoryDetail', {
         url: '/categoryDetail/:id',
-        templateUrl: '../html/categoryDetail.html',
+        templateUrl: '/html/categoryDetail.html',
         controller: 'categoryDetailController'
     }).state('order', {
         url: '/order',
-        templateUrl: '../html/order.html',
+        templateUrl: '/html/order.html',
         controller: 'orderController'
     }).state('search', {
         url: '/search',
-        templateUrl: '../html/search.html',
+        templateUrl: '/html/search.html',
         controller: 'searchController'
     });
     $urlRouterProvider.otherwise("/dashBoard");
